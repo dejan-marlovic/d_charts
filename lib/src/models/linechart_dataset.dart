f766a5f796a85ef54ef1ac220d21985f24b21c08
afpatmin <patrick.minogue@gmail.com>
2017-08-20 00:22:01 +0200

    Update linechart_dataset.dart

--- lib/src/models/linechart_dataset.dart
+++ lib/src/models/linechart_dataset.dart
@@ -6,38 +6,34 @@ class dLineChartDataset
 
   dLineChartDataset(this.dataPoints);
   dLineChartDataset.decode(Map<String, List<double>> data)
-  {
-    List<double> xValues = data['x'];
-    List<double> yValues = data['y'];
-    _validateChartData(xValues, yValues);
-
-    for (int i = 0; i < xValues.length; i++) dataPoints.add(new dDataPoint(xValues[i], yValues[i]));
-
+  {    
+    _validateChartData(data['x'], data['y']);
+    for (int i = 0; i < data['x'].length; i++) 
+    {
+      dataPoints.add(new dDataPoint(data['x'][i], data['y'][i]));
+    }
   }
 
   dLineChartDataset.fromJson(String data)
   {
-    print("decodeing");
     Map<String, List<double>> dataMap = JSON.decode(data);
-    List<double> xValues = dataMap['x'];
-    List<double> yValues = dataMap['y'];
     _validateChartData(dataMap['x'], dataMap['y']);
 
-    for (int i = 0; i < xValues.length; i++) dataPoints.add(new dDataPoint(xValues[i], yValues[i]));
+    for (int i = 0; i < dataMap['x'].length; i++) 
+    {
+      dataPoints.add(new dDataPoint(dataMap['x'][i], dataMap['y'][i]));
+    }
   }
 
   void _validateChartData(List<double> x, List<double> y)
   {
-    List<double> xValues = x;
-    List<double> yValues = y;
-
-    if (xValues == null || yValues == null) throw new FormatException("chart data must be a Map<String, List<double>> with 'x' and 'y as keys");
-    if (yValues.length != xValues.length) throw new FormatException("chart data x and y lists must be of equal length");
+    if (x == null || y == null) throw new FormatException("chart data must be a Map<String, List<double>> with 'x' and 'y as keys");
+    if (x.length != y.length) throw new FormatException("chart data x and y lists must be of equal length");
     
-    double xAxisInterval = xValues[1] - xValues[0];    
-    for (int i = 0; i < xValues.length-1; i++)
-    {     
-      if (xValues[i+1] - xValues[i] != xAxisInterval) throw new StateError("The interval between each x value must be constant!");
+    double xInterval = x[1] - x[0];    
+    for (int i = 0; i < x.length - 1; i++)
+    {
+      if (x[i+1] - x[i] != xInterval) throw new StateError("The interval between each x value must be constant!");
     }
   }
 
