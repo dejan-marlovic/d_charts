ede42113ff17a5f7808c11755431d1015e2ea56f
dejan-marlovic <dmarlovic83@gmail.com>
2017-08-20 00:51:39 +0200

    latest

--- lib/src/models/linechart_dataset.dart
+++ lib/src/models/linechart_dataset.dart
@@ -26,9 +26,8 @@ class dLineChartDataset
 
     if (xValues == null || yValues == null) throw new FormatException("chart data must be a Map<String, List<double>> with 'x' and 'y as keys");
     if (yValues.length != xValues.length) throw new FormatException("chart data x and y lists must be of equal length");
-    
-    double xAxisInterval = xValues[1] - xValues[0];    
-    for (int i = 0; i< xValues.length-1; i++)
+    double xAxisInterval = xValues[1] - xValues[0];
+    for (int i = 0; i < xValues.length-1; i++)
     {     
       if (xValues[i+1] - xValues[i] != xAxisInterval) throw new StateError("The interval between each x value must be constant!");
     }
